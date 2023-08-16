2b64b94bf7663eff1365e7fdc07eb4a979599c97
Sushant <128221017+Sushant212@users.noreply.github.com>
2023-08-17 00:54:14 +0545

    some edits

--- src/components/homepage/footer/Footer.jsx
+++ src/components/homepage/footer/Footer.jsx
@@ -1,24 +1,58 @@
 import React from "react";
 import logo from "../../resources/images/logo.png";
 import Data from "../../resources/Data";
+import { contactMethods } from "../Contact";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <footer className="bg-primary  h-full ">
-      <div className="container mx-auto px-4">
-        <div className="flex justify-around  text-white">
+      <div className="container mx-auto px-4 pt-10 ">
+        <div className="flex justify-around  text-white items-center">
           <img src={logo} alt="logos" className="h-20 w-18" />
-          <div>
+          <div className="">
             {Data.map((val, i) => {
               return (
-                <div key={i}>
-                  <div className="text-white capitalize hover:border-none hover:px-2 py-2 hover:rounded-lg px-2 hover:translate-x-3 hover:shadow-lg hover:shadow-white">
-                    <Link to={val.path}>{val.name}</Link>
+                <div key={i} className="">
+                  <div className="text-white py-1 text-sm capitalize hover:border-none hover:px-2  hover:rounded-lg px-2  hover:shadow-lg hover:shadow-white ">
+                    <Link to={val.path} className="py-1">
+                      {val.name}
+                    </Link>
                   </div>
                 </div>
               );
             })}
+          </div>
+          {/* join our community */}
+          <div className=" ">
+            <h2>Join Our Community</h2>
+            <div>
+              <input type="text" className="px-6 py-1.5 rounded-sm" />
+              <button
+                type="Submit"
+                value="submit"
+                className="border-2 ring-primary rounded-sm px-4 py-1"
+              >
+                Join
+              </button>
+            </div>
+          </div>
+          <div className="flex gap-6 ">
+            {contactMethods.map((val, i) => (
+              <a
+                key={i}
+                href={val.href}
+                target={val.target}
+
+                // className={`py-1 flex items-center gap-1 ${
+                //   hoveredIndex === i ? "bg-white" : ""
+                // }`}
+                // onMouseEnter={() => setHoveredIndex(i)}
+                // onMouseLeave={() => setHoveredIndex(null)}
+              >
+                <p className="cursor-pointer">{val.icon}</p>
+              </a>
+            ))}
           </div>
         </div>
         <div>
